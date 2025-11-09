ded4df401a518b8ce940bec8f7ad31dc3000bcc2
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2025-11-09 20:27:04 +0000

    Initialize TypeScript project with ESLint, Webpack, and Jest configuration. Add TypeScript files and a sample student list implementation.

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,38 @@
+interface Student{
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 20,
+  location: "New York",
+}
+
+const student2: Student = {
+  firstName: "Jane",
+  lastName: "Doe",
+  age: 21,
+  location: "Los Angeles",
+}
+
+const studentsList: Student[] = [student1, student2];
+
+const table = document.createElement('table');
+const tbody = document.createElement('tbody');
+table.appendChild(tbody);
+studentsList.forEach(student => {
+  const row = document.createElement('tr');
+  const cell1 = document.createElement('td');
+  const cell2 = document.createElement('td');
+  cell1.textContent = student.firstName;
+  cell2.textContent = student.location;
+  row.appendChild(cell1);
+  row.appendChild(cell2);
+  tbody.appendChild(row);
+});
+document.body.appendChild(table);
+
