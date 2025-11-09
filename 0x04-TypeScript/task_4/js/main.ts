fbdac8c03ba19ae1c1ac6bb6e67585ae73830a50
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2025-11-09 22:38:39 +0000

    Refactor main TypeScript file to improve instantiation of Cpp, Java, and React subjects by using local constants for better readability.

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -4,9 +4,13 @@
 /// <reference path="./subjects/Java.ts" />
 /// <reference path="./subjects/React.ts" />
 
-export const cpp = new Subjects.Cpp();
-export const java = new Subjects.Java();
-export const react = new Subjects.React();
+const Cpp = Subjects.Cpp;
+const Java = Subjects.Java;
+const React = Subjects.React;
+
+export const cpp = new Cpp();
+export const java = new Java();
+export const react = new React();
 
 export const cTeacher: Subjects.Teacher = {
   firstName: "John",
